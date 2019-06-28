6ae5cab3a605d38fa60c56741e683dcc7e2732e0
leitingting08 <leitingting08@gmail.com>
2019-06-28 09:46:35 +0800

    async

--- lib/pages/home/friends.dart
+++ lib/pages/home/friends.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:app/utils/net.dart';
 
-class MyFriends extends StatelessWidget {
+class MyFriends extends StatefulWidget {
+  MyFriends({Key key}) : super(key: key);
+
   @override
+  _MyFriendsState createState() => _MyFriendsState();
+}
+
+class _MyFriendsState extends State<MyFriends> {
+  //  NetRequest net;
+  //  var net = new NetRequest();
+  void _getInfo() async {
+    Map list = await NetRequest.get(
+        'https://movie.douban.com/typerank?type_name=%E7%A7%91%E5%B9%BB&type=17&interval_id=100:90&action=',
+        {});
+    print(list);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getInfo();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
